738d7c14d6fa4100dc3a2da20b74f0050d522ba3
nelml <konpaku.youmi@gmail.com>
2020-10-18 20:18:38 +0900

    refile secret_key config

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '54dc41c89ac474157f77e8a198315e6dfc91ab3da0e14005e0853b23d129cbe1effb13adfe0e6c3e33c90ce9c45385787d46f8432a29f976143b31fe0ab23c9a'
